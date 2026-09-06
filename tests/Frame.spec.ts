a10888399ac96b031d807b130816444fe79d0501
jatinjoshi1973 <jatinhari@yahoo.com>
2026-09-06 12:55:17 +0530

    Frames update

--- tests/Frame.spec.ts
+++ tests/Frame.spec.ts
@@ -1,7 +1,7 @@
 
-import {test, expect, Locator} from "@playwright/test"
+import {test, expect, Locator, Frame} from "@playwright/test"
 
-test.only('Frame actions', async({page})=>{
+test('Single iFrame', async({page})=>{
 
     await page.goto('https://demo.automationtesting.in/Frames.html');
     await page.waitForTimeout(2000);
@@ -17,6 +17,28 @@ test.only('Frame actions', async({page})=>{
      //by using framelocator
      const singleframe=page.frameLocator('#singleframe');
      singleframe?.locator("//input[@type='text']").fill('jatin');
+     await page.waitForTimeout(3000);
+
+
+    })
+
+    test.only('Nested iframe', async({page})=>{
+
+    await page.goto('https://demo.automationtesting.in/Frames.html');
+    await page.waitForTimeout(2000);
+    await page.getByRole('link', {name: 'Iframe with in an Iframe'}).click();
+    const outerframe=await page.frame({url:'https://demo.automationtesting.in/MultipleFrames.html'});
+    if(outerframe){
+        let innerFrames:Frame[]=outerframe.childFrames();
+        console.log("Number of frame are:", innerFrames?.length);
+        await innerFrames[0].locator("//input[@type='text']").fill('jatin');
+        await page.waitForTimeout(2000)
+    }
+    else{
+        console.log('Outer frame is null');
+        
+    }
+    
      await page.waitForTimeout(3000);
 
 
