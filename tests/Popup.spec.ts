c6c6bf8b4e6c380099f3279ea597df07580c00c9
jatinjoshi1973 <jatinhari@yahoo.com>
2026-09-13 14:20:16 +0530

    Popups

--- tests/Popup.spec.ts
+++ tests/Popup.spec.ts
@@ -30,7 +30,7 @@ test('Confirmation alert', async({page})=>{
 
 })
 
-test.only('Prompt alert', async({page})=>{
+test('Prompt alert', async({page})=>{
      page.on('dialog', async dialog => {
         
         console.log(dialog.message());
@@ -48,3 +48,45 @@ test.only('Prompt alert', async({page})=>{
      await page.waitForTimeout(3000);
 
 })
+
+test('Notification', async({page})=>{
+     
+
+     await page.goto('https://www.easemytrip.com/');
+     await page.waitForTimeout(6000);
+
+})
+
+test('Authentication pop up', async({page})=>{
+    
+     await page.goto('https://admin:admin@basic-auth-git-main-shashis-projects-4fa03ca5.vercel.app/');
+     await page.waitForTimeout(6000);
+
+})
+
+
+
+test('File upload', async({page})=>{
+    
+     await page.goto('https://www.ilovepdf.com/word_to_pdf');
+     const uploadelement:Locator = page.locator('[type="file"]')
+     uploadelement.setInputFiles('C:\\Users\\LENOVO\\Desktop\\playwrightword.docx')
+     await page.waitForTimeout(6000);
+
+})
+
+test.only('multipleFile upload', async({page})=>{
+    
+     await page.goto('https://davidwalsh.name/demo/multiple-file-upload.php');
+     const uploadelement:Locator = page.getByTestId('filesToUpload')
+     uploadelement.setInputFiles(['C:\\Users\\LENOVO\\Desktop\\playwrightword.docx', 'C:\\Users\\LENOVO\\Desktop\\Insurance claim.xlsx']);
+     expect(page.locator('#fileList li:nth-child(1)')).toHaveText('playwrightword.docx');
+     expect(page.locator('#fileList li:nth-child(2)')).toHaveText('Insurance claim.xlsx');
+     await page.waitForTimeout(2000);
+     uploadelement.setInputFiles([]);
+     expect(page.locator('#fileList li:nth-child(1)')).toHaveText('No Files Selected');
+     await page.waitForTimeout(4000);
+
+})
+
+
